cea6bd395e029b57da46a7fcc45b742b2a9c6a01
Kirill <kirillyat@gmail.com>
2019-11-04 00:32:06 +0300

    Update shell4.c

--- shell4.c
+++ shell4.c
@@ -9,16 +9,27 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-
-struct word{
+struct word {
     int value;
     struct word *next;
 };
 
-struct line{
+struct line {
     struct word *value;
     struct line *next;
+};
+
+struct command {
+    struct line *first;
+    int backgroundflag;
+    int inflag;
+    int outflag;
+    int appendflag;
+    int eofflag;
+    struct word* outfd;
+    struct word* infd;
 };
 
 void freeword(struct word *inputword)
@@ -42,6 +53,29 @@ void freeline(struct line* inputline)
     }
 }
 
+void freestring(char **input)
+{
+    if (input != NULL){
+        int i;
+        for (i = 0; input[i] != NULL; i++) {
+            free(input[i]);
+        }
+        free(input[i]);
+        free(input);
+    }
+}
+
+
+void freecommand(struct command* command)
+{
+    if (command != NULL) {
+        freeline(command->first);
+        freeword(command->infd);
+        freeword(command->outfd);
+        free(command);
+    }
+}
+
 void printword(struct word* inputword)
 {
     if (inputword != NULL) {
@@ -57,6 +91,16 @@ void printline(struct line* inputline)
         if (inputline->next != NULL)
             printf(" ");
         printline(inputline->next);
+    }
+}
+
+void printstring(char* const *output)
+{
+    if (output != NULL) {
+        int i;
+        for (i = 0; output[i] != NULL; i++)
+            printf("%s ", output[i]);
+        printf("\n");
     }
 }
 
@@ -82,31 +126,33 @@ int linelen(struct line *inputline)
 
 int ifWordIsReady(int symbol, int commaFlag)
 {
-    return ( ((symbol == ' ') && (!commaFlag)) || (symbol == '\n') || (symbol == EOF) || ((symbol == '&') && (!commaFlag)) );
+    return ( (symbol == '\n') ||
+             (symbol == EOF ) ||
+             ((symbol == ' ') && (!commaFlag)) ||
+             ((symbol == '&') && (!commaFlag)) ||
+             ((symbol == '>') && (!commaFlag)) ||
+             ((symbol == '<') && (!commaFlag)) ||
+             ((symbol == '|') && (!commaFlag)) );
 }
 
-struct word* readWord()
+int readWord(struct word** first)
 {
-    struct word *first = NULL, *last = NULL, *extra = NULL;
+    freeword(*first);
+    (*first) = NULL;
+    struct word *last = NULL;
     int symbol, commaFlag = 0;
     for (;;) {
         symbol = getchar();
         if (ifWordIsReady(symbol, commaFlag)) {
-            if ((symbol == '\n') && (commaFlag)){
+            if ((symbol == '\n') && (commaFlag))
                 symbol = '\"';
-            }
-            extra = malloc(sizeof(struct word));
-            extra->value = symbol;
-            extra->next = first;
-            first = extra;
-            break;
+            return symbol;
         } else if (symbol == '\"') {
             commaFlag = (commaFlag == 1)?0:1;
-            continue;
         } else {
-            if (first == NULL) {
-                first = malloc(sizeof(struct word));
-                last = first;
+            if (*first == NULL) {
+                *first = malloc(sizeof(struct word));
+                last = *first;
             } else {
                 last->next = malloc(sizeof(struct word));
                 last = last->next;
@@ -115,7 +161,7 @@ struct word* readWord()
             last->next = NULL;
         }
     }
-    return first;
+    return symbol;
 }
 
 void addWord(struct line **first, struct line **last, struct word* element)
@@ -131,66 +177,142 @@ void addWord(struct line **first, struct line **last, struct word* element)
     (*last)->next = NULL;
 }
 
-
-struct line* readCommand()
+void initCommand(struct command **input)
 {
-    struct line *first = NULL, *last = NULL;
-    struct word *buffer;
-    for (;;) {
-        buffer = readWord();
-        if (buffer == NULL) {
-            continue;
-        } else if (buffer->value == '\"') {
-            printf("\nerror: incorrect input {commas}\n");
-            freeword(buffer);
-            freeline(first);
-            first = NULL;
-            break;
-        } else if (buffer->value == EOF) {
-            if ((buffer->next != NULL) || (first != NULL)){
-                printf("\nerror: incorrect input {EOF}\n");
-                freeword(buffer->next);
-                buffer->next = NULL;
-            }
-            freeline(first);
-            addWord(&first, &last, buffer);
-            break;
-        } else if ((buffer->value == ' ') || (buffer->value == '\n') || (buffer->value == '&')) {
-            if (buffer->next != NULL)
-                addWord(&first, &last, buffer->next);
+    if (*input == NULL){
+        (*input) = malloc(sizeof(struct command));
+        (*input)->first = NULL;
+        (*input)->backgroundflag = 0;
+        (*input)->inflag = 0;
+        (*input)->outflag = 0;
+        (*input)->appendflag = 0;
+        (*input)->eofflag = 0;
+        (*input)->outfd = NULL;
+        (*input)->infd = NULL;
+    }
+}
+
+int lexicalAnalysis(struct command **input, int status)
+{
+    int analysis = 0;
+    
+    if ((*input)->backgroundflag == 1)
+        analysis = -1;
+    
+    if (status == '&') {
+        (*input)->backgroundflag = 1;
+    } else if (status == '<') {
         
-            if (buffer->value == ' '){
-                free(buffer);
-                continue;
-            } else if (buffer->value == '\n') {
-                free(buffer);
-                break;
-            } else if (buffer->value == '&'){
-                buffer->next = NULL;
-                addWord(&first, &last, buffer);
-                continue;
-            }
+        if ((*input)->inflag) {
+            analysis = -1;
+            printf("\nError: incorrect input {<}\n");
+        }
+        else
+            (*input)->inflag = 1;
+        
+    } else if (status == '>') {
+        if ((*input)->outflag) {
+            analysis = -1;
+            printf("\nError: incorrect input {> or >>}\n");
+        }
+        else
+            (*input)->outflag = 1;
+        
+    } else if (status == '\"') {
+        analysis = -1;
+        printf("\nError: incorrect input {commas}\n");
+    } else if (status == '\n') {
+         analysis = 1;
+    } else if (status == EOF) {
+        (*input)->eofflag = 1;
+        analysis = 1;
+        if ((*input)->first != NULL) {
+            printf("\nError: incorrect input {EOF}\n");
+            analysis = 0;
         }
     }
-    return first;
+    return analysis;
 }
 
-void freestring(char **input)
+int readLocation(struct command* result, int streem)
 {
-    int i;
-    for(i = 0; input[i] != NULL; i++){
-        free(input[i]);
+    int sucsess = 1, status;
+    struct word *buffer = NULL;
+    status = readWord(&buffer);
+    if (status != ' ') {
+        if ((buffer == NULL) && (status == streem) && (status == '>'))
+            result->appendflag = 1;
+        else
+            return !sucsess;
     }
-    free(input[i]);
-    free(input);
+    
+    
+    while (buffer == NULL) {
+        status = readWord(&buffer);
+        if ((status != ' ') && (buffer == NULL)){
+            if (status == EOF)
+                result->eofflag = 1;
+            return !sucsess;
+        }
+        
+    }
+       
+
+    if (streem == '>')
+        result->outfd = buffer;
+    else if (streem == '<') {
+        result->infd = buffer;
+    }
+    lexicalAnalysis(&result, status);
+    return sucsess;
 }
 
-void printstring( char* const *output)
+struct command* readCommand()
 {
-    int i;
-    for(i = 0; output[i] != NULL; i++)
-        printf("%s ", output[i]);
-    printf("\n");
+    struct command *result = NULL;
+    initCommand(&result);
+    struct line *first = NULL, *last = NULL;
+    struct word *buffer = NULL;
+    int status, analysis;
+    
+    for (;;) {
+        status = readWord(&buffer);
+        
+        if (buffer != NULL) {
+            addWord(&first, &last, buffer);
+            if (result->first == NULL)
+                result->first = first;
+            buffer = NULL;
+        }
+        
+        analysis = lexicalAnalysis(&result, status);
+        
+        if (analysis == 0){
+            if ((status == '>') || (status == '<'))
+                analysis = readLocation(result, status);
+        }
+        
+        if (analysis == -1) {
+            freecommand(result);
+            result = NULL;
+            break;
+        } else if (analysis == 1)
+            break;
+        
+        
+    }
+    return result;
+}
+
+char* word2string (struct word* list){
+    int wordsize = wordlen(list), i;
+    char *str = malloc(sizeof(char) * (1 + wordsize));
+    for (i = 0; i < wordsize; i++) {
+        str[i] = (char)list->value;
+        list = list->next;
+    }
+    str[wordsize] = 0;
+    return str;
 }
 
 char** list2string (struct line* list)
@@ -211,87 +333,108 @@ char** list2string (struct line* list)
         str[i][wordsize] = 0;
         list = list->next;
     }
+    
     return str;
 }
 
 int ifChangeDir(char** command)
 {
-    return ((command[0][0] == 'c') && (command[0][1] == 'd') && (command[0][2] == 0));
+    return ((command != NULL) &&
+            (command[0] != NULL) &&
+            (command[0][0] == 'c') &&
+            (command[0][1] == 'd') &&
+            (command[0][2] == 0) );
 }
 
-int ifBackground(struct line **command)
+int openIOfd(struct command* input)
 {
-    int rez = 0;
-    struct line *last = *command, *prev = *command;
-    while (last != NULL) {
-        struct word* tmp = last->value;
-        if ((tmp->value == '&') && (tmp->next == NULL)){
-            if ((rez == 0) && (last->next == NULL)) {
-                rez = '&';
-                free(prev->next);
-                prev->next = NULL;
-            } else {
-                rez = -1;
-                break;
-            }
+    int fd;
+    char* location = NULL;
+    
+    if (input->inflag) {
+        location = word2string(input->infd);
+        fd = open(location, O_RDONLY, 0666);
+        
+        if (fd == -1)
+            perror(location);
+        else {
+            dup2(fd, 0);
+            close(fd);
         }
-        prev = last;
-        last = last->next;
+        free(location);
     }
-    return rez;
+    
+    if (input->outflag) {
+        location = word2string(input->outfd);
+        if (input->appendflag)
+            fd = open(location, O_WRONLY|O_CREAT|O_APPEND, 0666);
+        else
+            fd = open(location, O_WRONLY|O_CREAT|O_TRUNC, 0666);
+        if (fd == -1)
+            perror(location);
+        else {
+            dup2(fd, 1);
+            close(fd);
+        }
+        free(location);
+    }
+    
+    return 0;
 }
 
 void сleaningZombieProcesses()
 {
-    while(wait4(-1, NULL, WNOHANG, NULL) > 0){}
-}
-
-
-void executeCommand(char** command, int mode)
-{
-    int rez, p, status, zombiePid;
-    сleaningZombieProcesses();
-    if (ifChangeDir(command)) {
-            rez = chdir(command[1]);
-            if (rez == -1)
-                perror(command[0]);
-    } else {
-        p = fork();
-        if (p == 0) {  /* CHILD */
-            execvp(command[0], command);
-            perror(command[0]);
-            exit(1);
-        }
-        if (mode != '&')
-            do {
-                zombiePid = wait(&status);//clean while pid !=p
-            }while (zombiePid != p);
+    while(wait4(-1, NULL, WNOHANG, NULL) > 0){
     }
 }
 
+void executeCommand(char **executeString, struct command *input)
+{
+    int rez, p, status, zombiePid;
+    сleaningZombieProcesses();
+    if (ifChangeDir(executeString)) {
+            rez = chdir(executeString[1]);
+            if (rez == -1)
+                perror(executeString[0]);
+    } else {
+        p = fork();
+        if (p == 0) {  /* CHILD */
+            openIOfd(input);
+            execvp(executeString[0], executeString);
+            perror(executeString[0]);
+            exit(1);
+        }
+        if (!input->backgroundflag)
+            do { /* clean while (pid != p) */
+                zombiePid = wait(&status);
+            } while (zombiePid != p);
+    }
+}
+
+
+
+
 void shell()
 {
-    struct line * input = NULL;
-    char **command;
+    struct command *input = NULL;
+    char** executeString = NULL;
     for (;;) {
         printf(">>> ");
         input = readCommand();
         if (input == NULL)
             continue;
-        else if ((input->value)->value == EOF) {
-            freeline(input);
+        else if (input->eofflag) {
+            freecommand(input);
             break;
         } else {
-            int mode = ifBackground(&input);
-            if (mode != -1){
-                command = list2string(input);
-                printstring(command);
-                executeCommand(command, mode);
-                freestring(command);
-            } else {
-                printf("error: incorrect input {&}");
-            }
-            freeline(input);
+            printline(input->first);
+            printf("\n");
+            executeString = list2string(input->first);
+            executeCommand(executeString, input);
+            
+            freestring(executeString);
+            executeString = NULL;
+            freecommand(input);
             input = NULL;
         }
     }
@@ -303,3 +446,4 @@ int main(int argc, const char * argv[])
     shell();
     return 0;
 }
+
