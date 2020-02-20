e980df4ed0d26daab013cfc1d6ca832c8c966974
Kirill <kirillyat@gmail.com>
2020-02-20 09:52:57 +0300

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,1 +1,70 @@
+//
+// robot.c
+// kirillyat
+// CMC MSU 2020
+//
 
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+class Player {
+    int number;
+    int money;
+    int raw;
+    int factory;
+    int product;
+    public:
+    PLayer(int N);
+    Player(int N, int M, int R, int F, int P);
+    void PrintStat();
+    void AddMoney(int S) { this->money+=S; }
+    void AddFactory() { this->factory++; }
+    void SetRaw(int R) { this->raw = raw; }
+    void SetProduct(int P) { this->product = P; }
+    void SetMoney(int M) { this->money = M; }
+    void SetFactory(int F) { this->factory = F; }
+}   
+
+
+class Game {
+    int level;
+    int prod;
+    public:
+        
+    void SetLevel(int L) { this->level = L; }
+    void UpLevel() { this->level++; }
+    void SetProd(int P) { this->prod = P}
+
+
+}
+
+
+class Bot: Player {
+    int sd;
+}
+
+
+Player::Player(int N, int M, int R, int F, int P)
+{
+    this->number = N;
+    this->money = M;
+    this->raw = R;
+    this->factory = F;
+    this->product = P;
+}
+
+
+void Player::PrintStat()
+{
+    printf("Player N");
+}
+
+
+
+
+int main ()
+{
+    return 0;
+}
